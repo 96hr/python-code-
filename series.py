f99addcedd012b7e3e470297d4ea3ff4ff859588
96hr <148626023+96hr@users.noreply.github.com>
2024-07-17 11:22:26 +0530

    Create series.py

---
+++ series.py
@@ -0,0 +1,10 @@
+print("Lowercase alphabets:")
+for char in range(ord('a'), ord('z')+1):
+    print(chr(char), end=' ')
+print() 
+
+
+print("Uppercase alphabets:")
+for char in range(ord('A'), ord('Z')+1):
+    print(chr(char), end=' ')
+print()  
